11c218513c0d4689b94640bb48b9512bf0774f41
yangyi <yangyirunning@163.com>
2019-07-13 18:30:27 +0800

    当当商品爬取

--- Requests.py
+++ Requests.py
@@ -1,6 +1,18 @@
 import requests
 
-r = requests.get("https://92.91p22.space/v.php?category=mf&viewtype=basic&page=53")
-print(r.status_code)
-r.encoding = "utf-8"
-print(r.text)
+
+# 爬虫通用代码框架
+
+def getHtmlText(url):
+    try:
+        r = requests.get(url)
+        print(r.status_code)
+        r.raise_for_status()  # 如果状态不是200，引发HTTPError异常
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "产生异常"
+
+if __name__=="__main__":
+    url = "https://92.91p22.space/v.php?category=mf&viewtype=basic&page=53"
+    print(getHtmlText(url))
---
+++ dangdangBook.py
@@ -0,0 +1,14 @@
+import requests
+
+def getDang(url):
+    try:
+        r = requests.get(url)
+        print(r.status_code)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text[:1000]
+    except:
+        print("爬取发生异常")
+
+if __name__ == "__main__":
+    print(getDang("http://search.dangdang.com/?key=%B5%CD%D3%FB%CD%FB%C9%E7%BB%E1&act=input"))
